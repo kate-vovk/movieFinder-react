230baaba6a1f24b9a30b2d2cd882940a46afad63
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-08-10 13:54:09 +0300

    add get post put delete methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import HTTPService from './services/httpService';
 
-HTTPService.get('movie/449406/credits').then((res: any) => {
-  console.log(res.data);
-});
-HTTPService.get('movie/2').then((res: any) => {
-  console.log(res.data);
-});
 export const App: FunctionComponent = () => {
-  return <div className="App">Test</div>;
+  return <div className="App" />;
 };
--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,15 +1,59 @@
-// eslint-disable-next-line import/extensions
-import axios from '../../node_modules/axios/index';
+const axios = require('axios').default;
 
 const API_KEY = '3f80d4cf4eb52d6e9d2ef400ea3d2acb';
-const BASE_URL = (path: string): string =>
+const BASE_URL = (path: string | number | void = ''): string =>
   `https://api.themoviedb.org/3/${path}?api_key=${API_KEY}`;
 
 export default class HTTPService {
-  static get(path: string): any {
-    return axios.get(BASE_URL(path)).then((response) => {
-      // console.log(response, response.data);
-      return response;
-    });
+  static get(path: string | number | void): any {
+    return axios({ method: 'get', url: BASE_URL(path) })
+      .then((response: any) => {
+        return response.data;
+      })
+      .catch((err: string) => {
+        throw new Error(err);
+      });
+  }
+
+  static post(data: any, path: string | number | void): any {
+    return axios({
+      method: 'post',
+      url: BASE_URL(path),
+      data: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response: string) => {
+        return response;
+      })
+      .catch((err: string) => {
+        throw new Error(err);
+      });
+  }
+
+  static put(data: any, path: string | number | void): any {
+    return axios({
+      method: 'put',
+      url: BASE_URL(path),
+      data,
+    })
+      .then((response: string) => {
+        return response;
+      })
+      .catch((err: string) => {
+        throw new Error(err);
+      });
+  }
+
+  static delete(path: string | number | void): any {
+    return axios({
+      method: 'delete',
+      url: BASE_URL(path),
+    })
+      .then((response: string) => {
+        return response;
+      })
+      .catch((err: string) => {
+        throw new Error(err);
+      });
   }
 }
