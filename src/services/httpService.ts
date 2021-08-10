8adc74972ae5225893492d2ccce7cda0396b1ee5
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-08-10 17:04:41 +0300

    fix mistakes

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,28 +1,12 @@
 const axios = require('axios').default;
 
-const API_KEY = '3f80d4cf4eb52d6e9d2ef400ea3d2acb';
-const BASE_URL = (path: string | number | void = ''): string =>
-  `https://api.themoviedb.org/3/${path}?api_key=${API_KEY}`;
+const BASE_URL = (path: string | number): string =>
+  `${process.env.REACT_APP_BASE_URL}${path}?api_key=${process.env.REACT_APP_API_KEY}`;
 
 export default class HTTPService {
-  static get(path: string | number | void): any {
+  static get(path: string | number = ''): any {
     return axios({ method: 'get', url: BASE_URL(path) })
       .then((response: any) => {
-        return response.data;
-      })
-      .catch((err: string) => {
-        throw new Error(err);
-      });
-  }
-
-  static post(data: any, path: string | number | void): any {
-    return axios({
-      method: 'post',
-      url: BASE_URL(path),
-      data: JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response: string) => {
         return response;
       })
       .catch((err: string) => {
@@ -30,13 +14,28 @@ export default class HTTPService {
       });
   }
 
-  static put(data: any, path: string | number | void): any {
+  static post(data: any, path: string | number = ''): any {
+    return axios({
+      method: 'post',
+      url: BASE_URL(path),
+      data,
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response: any) => {
+        return response;
+      })
+      .catch((err: string) => {
+        throw new Error(err);
+      });
+  }
+
+  static put(data: any, path: string | number): any {
     return axios({
       method: 'put',
       url: BASE_URL(path),
       data,
     })
-      .then((response: string) => {
+      .then((response: any) => {
         return response;
       })
       .catch((err: string) => {
@@ -44,12 +43,12 @@ export default class HTTPService {
       });
   }
 
-  static delete(path: string | number | void): any {
+  static delete(path: string | number): any {
     return axios({
       method: 'delete',
       url: BASE_URL(path),
     })
-      .then((response: string) => {
+      .then((response: any) => {
         return response;
       })
       .catch((err: string) => {
