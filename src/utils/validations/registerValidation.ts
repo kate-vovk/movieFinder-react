b53e20835cb1fb0d28138414b42dabb301f3afe9
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-08-10 12:21:33 +0300

    corrected some validation rules and added name and confirmPasswird fields

--- src/utils/validations/registerValidation.ts
+++ src/utils/validations/registerValidation.ts
@@ -1,9 +1,19 @@
 import * as yup from 'yup';
 
 export const validationSchema = yup.object({
+  name: yup
+    .string()
+    .matches(/'[a-zA-Z][a-zA-Z]+$'/, 'Must сontain ьin 2 сharacters, letters and/or dash'),
   email: yup.string().email('Enter a valid email').required('Email is required'),
   password: yup
     .string()
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
+    .required('Password is required')
+    .matches(
+      /'^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$'/,
+      'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
+    ),
+  confirmPassword: yup
+    .string()
+    .required()
+    .oneOf([yup.ref('password'), null], 'Passwords must match'),
 });
