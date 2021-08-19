7524f660a51d4f420676c13ef8e93255dbc9b02c
Viktor Beshentsev <viktor.beshentsev@nixsolutions.com>
2021-08-19 14:47:04 +0300

    main page update

--- src/components/MovieCard/styles.tsx
+++ src/components/MovieCard/styles.tsx
@@ -6,7 +6,7 @@ export const useStyle = makeStyles(() => ({
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
-  title: { textAlign: 'center' },
+  title: { textAlign: 'center', flex: '1 1 auto' },
   image: {
     width: '150px',
     height: '200px',
@@ -16,6 +16,20 @@ export const useStyle = makeStyles(() => ({
   description: {
     textAlign: 'justify',
   },
-  listItem: {},
-  item: {},
+  listItem: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    padding: '0px',
+  },
+  item: {
+    display: 'flex',
+    margin: '10px',
+    flex: '0 0 calc(33.333% - 20px)',
+    '@media (max-width:976px)': {
+      flex: '0 0 calc(50% - 20px)',
+    },
+    '@media (max-width:768px)': {
+      flex: '0 0 calc(100% - 20px)',
+    },
+  },
 }));
--- src/components/MoviesCards/styles.ts
+++ src/components/MoviesCards/styles.ts
@@ -1,12 +1,5 @@
 import { makeStyles } from '@material-ui/core';
 
 export const useStyle = makeStyles(() => ({
-  container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, 350px)',
-    gridTemplateRows: 'repeat(auto-fill, 450px)',
-    justifyContent: 'center',
-    margin: '10px',
-    gridGap: '20px',
-  },
+  container: {},
 }));
