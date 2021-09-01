eed2aa125e0c171566c0f9f53ef5ef9a7d97108f
Olha Pavlenko <pavlenko.olha@nixsolutions.com>
2021-09-01 12:32:21 +0000

    Feature/search

--- src/components/SearchBar/styles.ts
+++ src/components/SearchBar/styles.ts
@@ -2,7 +2,11 @@ import { makeStyles } from '@material-ui/core';
 
 export const useStyle = makeStyles(() => ({
   container: {
+    display: 'flex',
     margin: '5px 0',
     marginLeft: 'auto',
+  },
+  searchField: {
+    marginLeft: '5px',
   },
 }));
---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,30 @@
+import { Input, FormControl } from '@material-ui/core';
+import { ChangeEvent, FunctionComponent } from 'react';
+import InputLabel from '@material-ui/core/InputLabel';
+import { useStyle } from './styles';
+
+interface ISearchInputQuery {
+  searchQuery: string;
+  getSearchQuery: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const SearchInput: FunctionComponent<ISearchInputQuery> = ({
+  searchQuery,
+  getSearchQuery,
+}) => {
+  const classes = useStyle();
+
+  return (
+    <FormControl className={classes.searchForm}>
+      <InputLabel htmlFor="search">search</InputLabel>
+      <Input
+        type="text"
+        autoComplete="off"
+        placeholder="search"
+        value={searchQuery}
+        onChange={getSearchQuery}
+        id="search"
+      />
+    </FormControl>
+  );
+};
