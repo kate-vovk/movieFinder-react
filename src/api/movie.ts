565ce54520b9162d22b67912980557a7354b6e1e
Oleksii Zaika <oleksii.zaika@nixsolutions.com>
2021-09-01 13:42:58 +0000

    Feature/detail view

---
+++ src/api/movie.ts
@@ -0,0 +1,18 @@
+import { SERVER_PATHS } from '@/constants/constants';
+import HTTPService from '@/services/httpService';
+
+export const getMovie = (movieId: string): Promise<any> => {
+  return HTTPService.get(`${SERVER_PATHS.movies}/${movieId}`);
+};
+
+export const getListActors = (): Promise<any> => {
+  return HTTPService.get(SERVER_PATHS.actors);
+};
+
+export const getListGenres = (): Promise<any> => {
+  return HTTPService.get(SERVER_PATHS.genres);
+};
+
+export const getListCategories = (): Promise<any> => {
+  return HTTPService.get(SERVER_PATHS.categories);
+};
