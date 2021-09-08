4f36096777c266abccf7f7e41d360e9675d12836
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-09-08 12:24:27 +0300

    added else-construct

--- src/api/search.ts
+++ src/api/search.ts
@@ -16,8 +16,10 @@ export const getMovieByParams = (selectParam?: string, searchQuery?: string): Pr
         const finalMovies: Record<string, string | number>[] = [];
         movies.map((movie: Record<string, string | number>) => {
           // used negation because finalMovies is empty in the start of the condition
-          if (!finalMovies.some((finalMovie) => finalMovie.id === movie.id))
+          if (!finalMovies.some((finalMovie) => finalMovie.id === movie.id)) {
             finalMovies.push(movie);
+          }
+          return false;
         });
 
         return { data: finalMovies };
