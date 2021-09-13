a5377dd6aefa23c21c80421629b7ff63da18bafc
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-09-13 18:23:29 +0300

    removed switch for URL

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,17 +1,42 @@
+// import { searchOption } from '@/utils/interfaces/searchOption';
+// import { SERVER_PATHS } from '@/constants/constants';
+// import HTTPService from '@/services/httpService';
+// import { getMovies } from '@/businessLogic/movies';
+
+// export const getMovieByParams = (selectParam?: string, searchQuery?: string): Promise<any> => {
+//   switch (selectParam) {
+//     case searchOption.initial:
+//       return HTTPService.get(`${SERVER_PATHS.movies}?initial=${searchQuery}`);
+//     case searchOption.studio:
+//       return HTTPService.get(`${SERVER_PATHS.movies}?production_company=${searchQuery}`);
+//     case searchOption.actor:
+//       return HTTPService.get(`${SERVER_PATHS.movies}?actor=${searchQuery}`);
+//     default:
+//       return getMovies();
+//   }
+// };
+
 import { searchOption } from '@/utils/interfaces/searchOption';
 import { SERVER_PATHS } from '@/constants/constants';
 import HTTPService from '@/services/httpService';
-import { getMovies } from '@/businessLogic/movies';
 
 export const getMovieByParams = (selectParam?: string, searchQuery?: string): Promise<any> => {
+  let querySelectParam = '';
+  let queryPath = '';
   switch (selectParam) {
     case searchOption.initial:
-      return HTTPService.get(`${SERVER_PATHS.movies}?initial=${searchQuery}`);
+      querySelectParam = 'initial';
+      break;
     case searchOption.studio:
-      return HTTPService.get(`${SERVER_PATHS.movies}?production_company=${searchQuery}`);
+      querySelectParam = 'production_company';
+      break;
     case searchOption.actor:
-      return HTTPService.get(`${SERVER_PATHS.movies}?actor=${searchQuery}`);
+      querySelectParam = 'actor';
+      break;
     default:
-      return getMovies();
+      null;
   }
+
+  queryPath = `${SERVER_PATHS.movies}?${querySelectParam}=${searchQuery}`;
+  return HTTPService.get(queryPath);
 };
