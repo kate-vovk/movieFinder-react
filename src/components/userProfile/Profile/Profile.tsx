7553cddb91dd9cf172c619264a032e28263ee99d
Oleksii Zaika <zaika-o@bubuntu-lx.nixsolutions.com>
2021-09-14 15:32:05 +0300

    WIP

--- src/components/userProfile/Profile/Profile.tsx
+++ src/components/userProfile/Profile/Profile.tsx
@@ -23,7 +23,7 @@ export const Profile: FunctionComponent = () => {
     { setSubmitting }: FormikHelpers<IFormInputs>,
   ): Promise<void> => {
     // await dispatch(registration(values));
-    await console.log(values);
+    await console.log('name:', values.name, 'email:', values.email, 'birthday:', values.date);
     setSubmitting(false);
   };
 
@@ -62,62 +62,10 @@ export const Profile: FunctionComponent = () => {
             );
           })}
         </div>
-
         <Button color="primary" variant="contained" type="submit" disabled={formik.isSubmitting}>
           Save
         </Button>
       </form>
     </div>
-
-    // return (
-    //   <div>
-    //     <h1 className={classes.title}>Profile</h1>
-    //     <Formik
-    //       initialValues={{
-    //         userName: '',
-    //         email: '',
-    //         date: '',
-    //       }}
-    //       onSubmit={onHandleDataForCart}
-    //     >
-    //       {() => (
-    //         <Form>
-    //           <div className={classes.inputLine}>
-    //             <TextField
-    //               name="userName"
-    //               defaultValue={user.userName}
-    //               label="Name"
-    //               className={classes.input}
-    //               value={valueName}
-    //               onChange={handleChange}
-    //             />
-    //             <TextField
-    //               name="email"
-    //               defaultValue={user.userEmail}
-    //               label="Email"
-    //               className={classes.input}
-    //               value={valueEmail}
-    //               onChange={handleChange}
-    //             />
-    //             <TextField
-    //               name="date"
-    //               label="Birthday"
-    //               type="date"
-    //               value={valueDate}
-    //               onChange={handleChange}
-    //               defaultValue="0000-00-00"
-    //               className={classes.input}
-    //               InputLabelProps={{
-    //                 shrink: true,
-    //               }}
-    //             />
-    //           </div>
-    //           <Button color="primary" variant="contained" type="submit">
-    //             Save
-    //           </Button>
-    //         </Form>
-    //       )}
-    //     </Formik>
-    //   </div>
   );
 };
