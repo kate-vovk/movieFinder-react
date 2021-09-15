e84b4136343bb1cae23c40b7167379b7af6ff271
Olha Pavlenko <pavlenko-o@bubuntu-lx.nixsolutions.com>
2021-09-15 12:56:35 +0300

    fixed build bugs

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,10 +5,9 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    contentBasePublicPath: '/assets',
     open: true,
     historyApiFallback: true,
-    contentBase: './',
+    static: './',
     hot: true,
   },
 });
