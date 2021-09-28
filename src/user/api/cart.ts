b13b7be6bc37d04797fd3f2771a4df7d66e55a1f
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-09-28 12:52:10 +0300

    replace camel case with snake case in api/cart.ts

--- src/user/api/cart.ts
+++ src/user/api/cart.ts
@@ -5,7 +5,7 @@ import { IMyMovie, IOrderedMovie, IUser } from '@/interfaces/authInterfaces';
 import { ICartMovieState } from '@/interfaces/cartInterfaces';
 
 export const getCart = async (userId: string): Promise<any> => {
-  return HTTPService.get(`${SERVER_PATHS.cart}?userId=${userId}`);
+  return HTTPService.get(`${SERVER_PATHS.cart}?user_id=${userId}`);
 };
 
 export const addMovieToCart = async ({
@@ -14,7 +14,7 @@ export const addMovieToCart = async ({
   period,
   quality: quality_id,
 }: ICartMovieState): Promise<any> => {
-  return HTTPService.post(`${SERVER_PATHS.cart}?userId=${user_id}&filmId=${film_id}`, {
+  return HTTPService.post(`${SERVER_PATHS.cart}?user_id=${user_id}&film_id=${film_id}`, {
     user_id,
     film_id,
     period,
@@ -23,7 +23,7 @@ export const addMovieToCart = async ({
 };
 
 export const deleteMovieFromCart = async ({ movieId, userId }: ICartMovieState): Promise<any> => {
-  return HTTPService.delete(`${SERVER_PATHS.cart}?userId=${userId}&filmId=${movieId}`);
+  return HTTPService.delete(`${SERVER_PATHS.cart}?user_id=${userId}&film_id=${movieId}`);
 };
 
 // TODO will be modified when POST requests to Orders and MyMovies will be created
