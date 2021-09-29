0c074fb8f8207a16a9aff6d68338999909af9605
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-09-29 18:28:14 +0300

    change types in api

--- src/user/businessLogic/user.ts
+++ src/user/businessLogic/user.ts
@@ -1,7 +1,6 @@
-import { AxiosPromise } from 'axios';
 import { getUser as getUserAPI } from '@/user/api/user';
 
-export const getUser = async (userId: string): Promise<AxiosPromise> => {
+export const getUser = async (userId: string): Promise<any> => {
   const { data } = await getUserAPI(userId);
   return data;
 };
