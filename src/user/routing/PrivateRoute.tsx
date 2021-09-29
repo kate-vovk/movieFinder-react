08926a9fec101ece1cf6f3907c00ab341a9b0526
Kateryna Vovk <vovk-k@bubuntu-lx.nixsolutions.com>
2021-09-29 16:48:25 +0300

    fix/returnToPreviousRouteAfterLogin

--- src/user/routing/PrivateRoute.tsx
+++ src/user/routing/PrivateRoute.tsx
@@ -1,15 +1,16 @@
 import { useSelector } from 'react-redux';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useLocation } from 'react-router-dom';
 import { FunctionComponent } from 'react';
 import { IPrivateRouteProps } from '@/interfaces/authInterfaces';
-import { userSelector } from '@/user/store/selectors/auth';
+import { userIdSelector } from '@/user/store/selectors/auth';
 import { CLIENT_PATHS } from '@/user/constants';
 
 export const PrivateRoute: FunctionComponent<IPrivateRouteProps> = ({ path, component }) => {
-  const isLoggedIn = useSelector(userSelector);
+  const isLoggedIn = useSelector(userIdSelector);
+  const location = useLocation();
   return isLoggedIn ? (
     <Route exact path={path} component={component} />
   ) : (
-    <Redirect to={CLIENT_PATHS.signin} />
+    <Redirect to={{ pathname: CLIENT_PATHS.signin, state: { prevPath: location.pathname } }} />
   );
 };
